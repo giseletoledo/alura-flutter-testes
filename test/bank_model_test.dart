29b06f4625d136d021ce0964c2f95ecb80ea5896
Gisele <gisesonia@gmail.com>
2022-12-04 13:28:11 -0300

    Testes de tap

--- test/bank_model_test.dart
+++ test/bank_model_test.dart
@@ -1,16 +1,14 @@
 import 'package:estilizacao_componentes/models/bank.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main()
-{
-
+void main() {
   group('Bank model deposit tests', () {
     test('Bank model deposit shoul be 10', () {
       final bank = BankModel();
       bank.deposit(10);
       expect(bank.points, 10);
-
     });
+
     test('Bank model deposit shoul turn available into 10', () {
       final bank = BankModel();
       bank.transfer(10);
@@ -18,7 +16,19 @@ void main()
     });
   });
 
+  //desafio 2
+  test('Bank model should start with 0', () {
+    final bank = BankModel();
+    expect(bank.available, 0);
+  });
 
+  //desafio 3
+  test('Bank model should have 100', () {
+    final bank = BankModel();
+    bank.deposit(200);
+    bank.transfer(100);
+    expect(bank.spent, 100);
+  });
 
   test('Bank model transfer should turn points into 10', () {
     final bank = BankModel();
--- test/home_test.dart
+++ test/home_test.dart
@@ -1,3 +1,4 @@
+import 'package:estilizacao_componentes/components/box_card.dart';
 import 'package:estilizacao_componentes/data/bank_inherited.dart';
 import 'package:estilizacao_componentes/screens/home.dart';
 import 'package:flutter/material.dart';
@@ -5,11 +6,13 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main(){
   testWidgets('My widget has a text "Spent" ', (widgetTester) async{
+    //constrói a tela que será testada
     await widgetTester.pumpWidget(MaterialApp(
         home: BankInherited(
             child: Home()
         ),
        ));
+    //busca o texto no widget
     final spentFinder = find.text('Spent');
     expect(spentFinder, findsOneWidget);
   });
@@ -20,6 +23,65 @@ void main(){
       ),
     ));
     expect(find.byType(LinearProgressIndicator), findsOneWidget);
+  });
+
+  //Busca 5 BoxCards
+  testWidgets('finds 5 BoxCards ', (widgetTester) async {
+    await widgetTester.pumpWidget(MaterialApp(
+      home: BankInherited(
+          child: Home()
+      ),
+    ));
+    expect(find.byWidgetPredicate((widget){
+      if(widget is BoxCard){
+        return true;
+      } else  {
+        return false;
+      }
+    }), findsNWidgets(5));
+  });
+  //Busca 5 BoxCards
+  testWidgets('finds 5 BoxCards ', (widgetTester) async {
+    await widgetTester.pumpWidget(MaterialApp(
+      home: BankInherited(
+          child: Home()
+      ),
+    ));
+    expect(find.byWidgetPredicate((widget){
+      if(widget is BoxCard){
+        return true;
+      } else  {
+        return false;
+      }
+    }), findsNWidgets(5));
+  });
+
+  //desafio 1
+  testWidgets('My widget has a text "Transfer" ', (widgetTester) async{
+    //constrói a tela que será testada
+    await widgetTester.pumpWidget(MaterialApp(
+      home: BankInherited(
+        child: Home(),
+      ),
+    ));
+    //busca o texto no widget
+    final transferFinder = find.text('Transfer');
+    expect(transferFinder, findsOneWidget);
+  });
+
+  testWidgets('When tap Deposit should upload earned in 10', (tester) async {
+    //constrói a tela que será testada
+    await tester.pumpWidget(MaterialApp(
+      home: BankInherited(
+        child: Home(),
+      ),
+    ));
+    await tester.tap(find.text('Deposit'));//ao fazer o tap muda para 10
+    await tester.tap(find.text('Earned'));
+
+    await tester.pumpAndSettle();// obrigatório para atualizar a tela
+
+    expect(find.text('\$10.0'), findsOneWidget);//verifica se tem o valor 10
   });
 
 }
